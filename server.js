aa37b5178a0adc64bf452ab67b3e9cddfb4682f9
Jordan Shipley <jordan.j.shipley@gmail.com>
2020-12-05 16:40:45 -0700

    schema.sql

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
+//require dependencies
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+//create connection
 const connection = mysql.createConnection({
     host: "local host",
+    //port
     port: 3306,
+    //username
     user: "root",
+    //password
     password: "",
+    //database name
     database:"employee_db",
 });
+
+//
+connection.connect(function(err) {
+    if (err) throw err;
+    runSearch();
+});
+
+function runSearch() {
+    inquirer
+        .prommpt({
+            
+        })
+}
