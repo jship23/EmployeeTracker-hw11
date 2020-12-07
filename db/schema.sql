b70db201dd99deb4781fe5382f7315f262c92c43
Jordan Shipley <jordan.j.shipley@gmail.com>
2020-12-06 17:32:18 -0700

    adjusted the schema

--- db/schema.sql
+++ db/schema.sql
@@ -10,7 +10,8 @@ last_name VARCHAR (30),
 role_id INT,
 manager_id INT,
 PRIMARY KEY (id),
-FOREIGN KEY (role_id, manager_id)
+FOREIGN KEY (role_id) REFERENCES role(id),
+FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
 CREATE TABLE role (
@@ -19,11 +20,11 @@ title VARCHAR (30),
 salary DECIMAL,
 department_id INT,
 PRIMARY KEY (id),
-FOREIGN KEY (department_id)
+FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE department(
 id INTEGER NOT NULL AUTO_INCREMENT,
 name VARCHAR (30),
-PRIMARY KEY (id),
+PRIMARY KEY (id)
 );
